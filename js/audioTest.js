610383e6a36d1e534ab15ed1d5df39a884fac4bc
Brian Wang <bubwb12@gmail.com>
2024-11-08 11:54:59 -0500

    hotfix 8

--- js/audioTest.js
+++ js/audioTest.js
@@ -1,7 +1,7 @@
 const findSpaceBar = document.getElementById('findSpaceBar');
 
 document.addEventListener('click', () => {
-    const audio = document.getElementById("myAudio");
+    const audio = document.getElementById("findSpaceBar");
     audio.play().catch(error => {
         console.error("Audio play was blocked:", error);
     });
